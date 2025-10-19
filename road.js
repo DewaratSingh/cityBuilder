fb8ab8a2c7f9bd0223ecc08e2ddbfc6794619cd2
DewaratSingh <mamtasingh77099@gmail.com>
2025-10-19 14:56:11 +0530

    road pathway problem solve

--- road.js
+++ road.js
@@ -234,31 +234,91 @@ class Road {
       this.segments[length].endingPosition.x,
       this.segments[length].endingPosition.y
     );
-let C = Vector.sub(B, A);
-let Cmag = C.mag();
-C.normalize();
+    let C = Vector.sub(B, A);
+    let Cmag = C.mag();
+    C.normalize();
 
-let mag = 25;
-while (mag <= Cmag) {
-  let step = C.copy().mult(mag);
-  let D = Vector.add(A, step);
-  let perpLeft = createVector(-C.y, C.x);
-  perpLeft.setMag(60);
-  let finalPosLeft = Vector.add(D, perpLeft);
-  this.areaZone.push(new AreaZone(finalPosLeft.x, finalPosLeft.y, 50, 50, C.angle()));
+    let mag = 25;
+    while (mag <= Cmag) {
+      let step = C.copy().mult(mag);
+      let D = Vector.add(A, step);
+      let perpLeft = createVector(-C.y, C.x);
+      perpLeft.setMag(60);
+      let finalPosLeft = Vector.add(D, perpLeft);
+      this.areaZone.push(
+        new AreaZone(finalPosLeft.x, finalPosLeft.y, 50, 50, C.angle())
+      );
 
-  let perpRight = createVector(C.y, -C.x);
-  perpRight.setMag(60);
-  let finalPosRight = Vector.add(D, perpRight);
-  this.areaZone.push(new AreaZone(finalPosRight.x, finalPosRight.y, 50, 50, C.angle()));
+      let perpRight = createVector(C.y, -C.x);
+      perpRight.setMag(60);
+      let finalPosRight = Vector.add(D, perpRight);
+      this.areaZone.push(
+        new AreaZone(finalPosRight.x, finalPosRight.y, 50, 50, C.angle())
+      );
 
-  mag += 55;
-}
-
-
-
-    
+      mag += 55;
+    }
+    this.zoneDeletor();
   }
+
+  zoneDeletor() {
+    for (let i = this.areaZone.length - 1; i >= 0; i--) {
+      for (let j = i - 1; j >= 0; j--) {
+        let d = distance(
+          this.areaZone[i].position.x,
+          this.areaZone[i].position.y,
+          this.areaZone[j].position.x,
+          this.areaZone[j].position.y
+        );
+        if (d < 50) {
+          this.areaZone.splice(i, 1);
+          break;
+        }
+      }
+    }
+
+    for (let i = this.areaZone.length - 1; i >= 0; i--) {
+      const circle = this.areaZone[i];
+      const P = circle.position;
+      const r = 50;
+
+      let collided = false;
+
+      for (let j = 0; j < this.segments.length; j++) {
+        const seg = this.segments[j];
+        const A = seg.startingPosition;
+        const B = seg.endingPosition;
+
+        const AB = { x: B.x - A.x, y: B.y - A.y };
+        const AP = { x: P.x - A.x, y: P.y - A.y };
+
+        const ab2 = AB.x * AB.x + AB.y * AB.y;
+        const ap_dot_ab = AP.x * AB.x + AP.y * AB.y;
+
+        let t = ap_dot_ab / ab2;
+        t = Math.max(0, Math.min(1, t));
+
+        const C = {
+          x: A.x + AB.x * t,
+          y: A.y + AB.y * t,
+        };
+
+        const dx = P.x - C.x;
+        const dy = P.y - C.y;
+        const dist = Math.sqrt(dx * dx + dy * dy);
+
+        if (dist <= r) {
+          collided = true;
+          break;
+        }
+      }
+
+      if (collided) {
+        this.areaZone.splice(i, 1);
+      }
+    }
+  }
+
   createEndSegmentMove(x, y, w = 50, color = "grey") {
     if (this.segments.length > 0 && this.changeable) {
       this.segments[this.segments.length - 1].endingPosition = { x, y };
